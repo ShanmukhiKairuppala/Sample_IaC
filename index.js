61e64da6b0a63f1006807a1ee3385f9c242405c4
Shanmukhi Kairuppala <shanmukhikairuppala@gmail.com>
2024-05-17 12:05:54 +0530

    Hello World Nodejs file

---
+++ index.js
@@ -0,0 +1,13 @@
+const PORT = 3500;
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
